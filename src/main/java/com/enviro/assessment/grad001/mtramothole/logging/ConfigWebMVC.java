9859007915634752f4c0ff3f89d81ae101e18e54
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-16 00:35:30 +0200

    Register a interceptor

---
+++ src/main/java/com/enviro/assessment/grad001/mtramothole/logging/ConfigWebMVC.java
@@ -0,0 +1,16 @@
+package com.enviro.assessment.grad001.mtramothole.logging;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ConfigWebMVC implements WebMvcConfigurer {
+    //    Registers the LoggingInterceptor class as an interceptor
+//    The LoggingInterceptor class logs information about the request and response
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggingInterceptor());
+    }
+}
