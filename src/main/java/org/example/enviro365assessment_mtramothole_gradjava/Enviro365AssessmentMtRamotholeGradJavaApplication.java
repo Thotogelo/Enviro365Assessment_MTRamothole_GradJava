7d0e4da2940caa82280bca8596698ea41d91bad5
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-15 21:09:41 +0200

    initial commit

---
+++ src/main/java/org/example/enviro365assessment_mtramothole_gradjava/Enviro365AssessmentMtRamotholeGradJavaApplication.java
@@ -0,0 +1,13 @@
+package org.example.enviro365assessment_mtramothole_gradjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Enviro365AssessmentMtRamotholeGradJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Enviro365AssessmentMtRamotholeGradJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/enviro365assessment_mtramothole_gradjava/Enviro365AssessmentMtRamotholeGradJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.enviro365assessment_mtramothole_gradjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Enviro365AssessmentMtRamotholeGradJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
