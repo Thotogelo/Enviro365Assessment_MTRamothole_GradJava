ee389a0b5cc47c24efe8c894293cbe2613e7c60d
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-16 13:22:14 +0200

    Radical change

--- src/main/java/com/enviro/assessment/grad001/mtramothole/service/WasteServiceImp.java
+++ src/main/java/com/enviro/assessment/grad001/mtramothole/service/WasteServiceImp.java
@@ -1,10 +1,13 @@
 package com.enviro.assessment.grad001.mtramothole.service;
 
+import com.enviro.assessment.grad001.mtramothole.exception.WasteNotFoundException;
 import com.enviro.assessment.grad001.mtramothole.model.Waste;
 import com.enviro.assessment.grad001.mtramothole.repository.WasteRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class WasteServiceImp implements WasteService {
@@ -15,36 +18,72 @@ public class WasteServiceImp implements WasteService {
         this.wasteRepository = wasteRepository;
     }
 
+    // Method to save a waste object. Returns the saved waste object.
     @Override
-    public int saveWaste(Waste waste) {
-        return wasteRepository.saveWaste(waste);
+    @Transactional
+    public Waste saveWaste(Waste waste) {
+        return wasteRepository.save(waste);
     }
 
+    // Save can be used to update as well
+    // Method to update a waste object. Returns the updated waste object.
     @Override
-    public int deleteWasteById(Long id) {
-        return wasteRepository.deleteWasteById(id);
+    @Transactional
+    public Waste updateWaste(Waste waste) {
+        return wasteRepository.save(waste);
     }
 
-    @Override
-    public int updateWaste(Waste waste) {
-        return wasteRepository.updateWaste(waste);
-    }
-
+    // Method to find a waste object by its id. Returns the found waste object.
+    // exception is thrown if waste is not found and handled in the global exception
+    // handler
     @Override
     public Waste findWasteById(Long id) {
-        return wasteRepository.findWasteById(id);
+        return Optional.ofNullable(wasteRepository.findWasteById(id))
+                .orElseThrow(() -> new WasteNotFoundException("Waste with id " + id + " not found."));
     }
 
+    // Method to remove a waste object by its id.
+    // exception is thrown if waste is not found and handled in the global exception
+    // handler
     @Override
-    public int deleteWasteByWastecategory(String wastecategory) {
-        return wasteRepository.deleteWasteByWastecategory(wastecategory);
+    @Transactional
+    public void removeWasteById(Long id) {
+        int rowsAffected = wasteRepository.deleteWasteById(id);
+        if (rowsAffected == 0) {
+            throw new WasteNotFoundException("Waste with id " + id + " not found.");
+        }
     }
 
+    // Method to remove waste objects by their category.
+    // exception is thrown if waste is not found and handled in the global exception
+    // handler
     @Override
-    public int findWasteByWastecategory(String wastecategory) {
-        return wasteRepository.findWasteByWastecategory(wastecategory);
+    @Transactional
+    public void removeWasteByWastecategory(String category) {
+        int rowsAffected = wasteRepository.deleteWasteByWastecategory(category);
+        if (rowsAffected == 0) {
+            throw new WasteNotFoundException("Waste list with category " + category + " not found.");
+        }
     }
 
+    // Method to find waste objects by their category. Returns a list of found waste
+    // objects.
+    // exception is thrown if waste is not found and handled in the global exception
+    // handler
+    @Override
+    public List<Waste> findWasteByWastecategory(String category) {
+        List<Waste> wasteList = wasteRepository.findWasteByWastecategory(category);
+        if (wasteList != null) {
+            return wasteList;
+        } else {
+            throw new WasteNotFoundException("Waste lists with category " + category + " not found.");
+        }
+    }
+
+    // Method to find all waste objects. Returns a list of all waste objects.
+    // exception is thrown if waste is not found and handled in the global exception
+    // handler
+    @Override
     public List<Waste> findAll() {
         return wasteRepository.findAll();
     }
