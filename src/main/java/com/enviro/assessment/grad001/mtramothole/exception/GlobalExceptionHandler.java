8ec3644bd4dbfb70a73ff7578643ae5ce931ee74
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-16 14:34:14 +0200

    comments

--- src/main/java/com/enviro/assessment/grad001/mtramothole/exception/GlobalExceptionHandler.java
+++ src/main/java/com/enviro/assessment/grad001/mtramothole/exception/GlobalExceptionHandler.java
@@ -12,12 +12,17 @@ public class GlobalExceptionHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
+
+    // This method handles all types of exceptions.
+    // It logs the error and returns a generic error message with a status of INTERNAL_SERVER_ERROR.\
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception e) {
         logger.error("Error occurred", e);
         return new ResponseEntity<>("An error occurred while processing the request.", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    // This method specifically handles WasteNotFoundException.
+    // It returns the exception's message with a status of NOT_FOUND.
     @ExceptionHandler(WasteNotFoundException.class)
     public ResponseEntity<String> handleWasteNotFoundException(WasteNotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
