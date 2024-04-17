e459a1f07ecd8810a840b7fddcfe3f6d221efb7b
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-17 21:29:45 +0200

    Refactor exception handling in GlobalExceptionHandler

--- src/main/java/com/enviro/assessment/grad001/mtramothole/exception/GlobalExceptionHandler.java
+++ src/main/java/com/enviro/assessment/grad001/mtramothole/exception/GlobalExceptionHandler.java
@@ -3,28 +3,30 @@ package com.enviro.assessment.grad001.mtramothole.exception;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-
     // This method handles all types of exceptions.
-    // It logs the error and returns a generic error message with a status of INTERNAL_SERVER_ERROR.\
+    // It logs the error and returns a generic error message with a status of
+    // INTERNAL_SERVER_ERROR.\
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception e) {
+    public ProblemDetail handleException(Exception e) {
         logger.error("Error occurred", e);
-        return new ResponseEntity<>("An error occurred while processing the request.", HttpStatus.INTERNAL_SERVER_ERROR);
+        return ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR,
+                "An error occurred while processing the request.");
     }
 
     // This method specifically handles WasteNotFoundException.
     // It returns the exception's message with a status of NOT_FOUND.
     @ExceptionHandler(WasteNotFoundException.class)
-    public ResponseEntity<String> handleWasteNotFoundException(WasteNotFoundException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    public ProblemDetail handleWasteNotFoundException(WasteNotFoundException ex) {
+        logger.error("Waste not found", ex);
+        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
     }
 }
