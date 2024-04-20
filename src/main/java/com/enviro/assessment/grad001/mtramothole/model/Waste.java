b7d157bad198c5eed300fa9a72c1658ef9355bc5
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-04-20 12:23:10 +0200

    Improve lombok

--- src/main/java/com/enviro/assessment/grad001/mtramothole/model/Waste.java
+++ src/main/java/com/enviro/assessment/grad001/mtramothole/model/Waste.java
@@ -1,48 +1,30 @@
 package com.enviro.assessment.grad001.mtramothole.model;
 
-import java.io.Serializable;
-
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-@AllArgsConstructor
+@Data
 @NoArgsConstructor
-@Getter
-@Setter
 @Entity
 @Table(name = "waste")
-public class Waste implements Serializable {
+public class Waste {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @NotNull
     @Column(name = "waste_id")
     private Long id;
 
-    @NotNull
-    @NotEmpty
-    @NotBlank
     @Column(name = "waste_category")
     @Size(min = 1, max = 50, message = "Waste category must be between 1 and 50 characters")
     private String wastecategory;
 
-    @NotNull
-    @NotEmpty
-    @NotBlank
     @Column(name = "disposal_guideline")
     @Size(min = 1, max = 50, message = "Disposal guidelines must be between 1 and 50 characters")
     private String disposalguideline;
 
-    @NotNull
-    @NotEmpty
-    @NotBlank
-    @Size(min = 1, max = 50, message = "Recycling tips must be between 1 and 50 characters")
     @Column(name = "recycling_tips")
+    @Size(min = 1, max = 50, message = "Recycling tips must be between 1 and 50 characters")
     private String recyclingtip;
 }
